c8351cbfff96b429f5368d4f3aa2bff1568e328c
Robert <rr.zinyat@gmail.com>
2021-10-16 23:11:24 +0300

    added comparsions

--- Module_07/ex00/Templates.hpp
+++ Module_07/ex00/Templates.hpp
@@ -1,27 +1,25 @@
 template < typename T >
-class templates {
-public:
-	max
-	min
-	swap
-};
-
-
-
+const T& max( const T& a, const T& b ) { return ( a >=b ) ? a : b; }
 template < typename T >
-const T& max( const T& a, const T& b) {
-	return ( a >=b ) ? a : b;
-}
-
-template < typename T >
-const T& min( const T& a, const T& b) {
-	return ( a < b ) ? a : b;
-}
+const T& min ( const T& a, const T& b ) { return ( a < b ) ? a : b; }
 
 template < typename T >
 void swap( T& a, T& b) {
 	T c = a;
-
 	a = b;
 	b = c;
 }
+
+template <typename T>
+class Comparable {
+	private:
+		T _data;
+	public:
+		Comparable (const T& rhs) : _data(rhs) {}
+		bool operator>	( const Comparable<T>& rhs ) { return ( this->_data > rhs._data ); }
+		bool operator<	( const Comparable<T>& rhs ) { return ( this->_data < rhs._data ) ; }
+		bool operator>=	( const Comparable<T>& rhs ) { return ( this->_data >= rhs._data ); }
+		bool operator<=	( const Comparable<T>& rhs ) { return ( this->_data <= rhs._data ); }
+		bool operator==	( const Comparable<T>& rhs ) { return ( this->_data == rhs._data ); }
+		bool operator!=	( const Comparable<T>& rhs ) { return !( this->_data == rhs._data ); }
+};
--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -4,18 +4,33 @@
 int main( void ) {
 	int a = 2;
 	int b = 3;
-	
+
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	
-	::swap(c, d);
+
+	::swap( c, d );
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	Comparable <int>n( 20 );
+	Comparable <int>m( 20 );
+
+	// Comparable <float>n( 42.05f );
+	// Comparable <float>m( 20.15f );
+
+	std::cout << std::endl;
+	std::cout << (n > m) << std::endl;
+	std::cout << (n < m) << std::endl;
+	std::cout << (n >= m) << std::endl;
+	std::cout << (n <= m) << std::endl;
+	std::cout << (n == m) << std::endl;
+	std::cout << (n != m) << std::endl;
+
 	return 0;
 }
