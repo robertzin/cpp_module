d49a12a53cc448f6f2072644829973be478a5d82
Robert <rr.zinyat@gmail.com>
2021-10-20 21:59:27 +0300

    fixed

--- Module_08/ex02/mutantstack.hpp
+++ Module_08/ex02/mutantstack.hpp
@@ -5,35 +5,22 @@
 # include <iterator>
 # include <iostream>
 
-template < typename T >
+template<typename T>
 class MutantStack : public std::stack<T> {
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs)
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
 
-	public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
 
-		typedef typename std::stack<T>::container_type::iterator				iterator;
-		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
-		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
-		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
-
-		MutantStack() : std::stack<T>() {};
-		MutantStack( const MutantStack& toCopy ) : std::stack<T>(toCopy) {};
-		virtual ~MutantStack() {};
-
-		MutantStack& operator=( const MutantStack& other ) {
-			if ( *this == other )
-				return *this;
-			std::stack<T>::operator=( other );
-			return *this;
-		}
-
-		iterator begin() { return (std::stack<T>::c.begin()); }
-		iterator end() { return (std::stack<T>::c.end()); }
-		const_iterator begin() const { return (std::stack<T>::c.begin()); }
-		const_iterator end() const { return (std::stack<T>::c.end()); }
-		reverse_iterator rbegin() { return (std::stack<T>::c.rbegin()); }
-		reverse_iterator rend() { return (std::stack<T>::c.rend()); }
-		const_reverse_iterator rbegin() const { return (std::stack<T>::c.rbegin()); }
-		const_reverse_iterator rend() const { return (std::stack<T>::c.rend()); }
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
 };
 
 #endif
